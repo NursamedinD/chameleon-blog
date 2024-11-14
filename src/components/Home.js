362205c7e32713d82983c122e32fdd7445e61ee3
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-11-14 04:35:58 +0300

    added code to Home to list posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Home({ posts }) {
   return (
-    <div>Home</div>
+    <div>
+      <h2>New Posts</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <Link to={`/post/${post.id}`}>
+              <h3>{post.title}</h3>
+              <img src={post.image} alt={post.title} style={{ width: '100px' }} />
+              <p>{post.content.slice(0, 100)}... <strong>Read More</strong></p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
