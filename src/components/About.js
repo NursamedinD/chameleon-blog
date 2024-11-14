474640dead03ebf6bb55398bf944ee30943eddfc
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-11-15 02:10:23 +0300

    centering of footer and about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export function About() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
         <h2>About this Blog</h2>
-        <p>Welcome to this blog, in this blog you'll find some neat facts and information about chameleons. 
-        I picked chameleons because they were my favorite animal growing up as a kid, 
-        so i made this blog dedicated to information about them</p>
+        <p>Welcome to this blog, in this blog you'll find some neat facts and information about chameleons.</p> 
+        <p>I picked chameleons because they were my favorite animal growing up as a kid,</p>
+        <p>so i made this blog dedicated to information about them</p>
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
+    <div style={{ textAlign: "center" }}>
     <footer>
       <p>Nursamedin Abdi Dualle Chameleon Blog.&copy; All rights reserved.</p>
     </footer>
+    </div>
   )
 }
 
