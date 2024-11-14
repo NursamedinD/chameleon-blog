62bad14b3d648e8a37b1bc75c84569435a33941c
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-11-14 03:21:52 +0300

    adding router and usestate/useeffect to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Header from "./components/Header";
 import About from "./components/About";
 import Home from "./components/Home";
 import NewPostForm from "./components/NewPostForm";
 import PostDetail from "./components/PostDetail";
 import Footer from "./components/Footer";
-// import { Router,Routes } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [posts, setPosts] = useState ([]);
+
+  useEffect(() => {
+    fetch()
+    .then(response => response.json())
+    .then(data => setPosts(data))
+  }, [])
+
+  function addNewPost(post) {
+    setPosts([...posts, { ...post, id: posts.length + 1 }]);
+  }
+
   return (
-    <div>
+    <Router>
+          <div>
+      <Header />
       <h1 style={{ textAlign: "center", color: "lightgreen" }}>
         <span style={{ background: "darkgreen" }}>Chameleon Blog</span>
       </h1>
-      <main>
-        <About />
-        <Home />
-        <NewPostForm />
-        <PostDetail />
-      </main>
+      <Routes>
+        <Route path="/" element={<Home posts={posts} />} />
+        <Route path="/post/:postId" element={<PostDetail posts={posts} />} />
+        <Route path="/new" element={<NewPostForm addNewPost={addNewPost} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </nav>
+    </header>
   )
 }
 
